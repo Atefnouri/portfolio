56be5b98f31206b74c76e5892d45fc2d5dd0ad7b
Atef Nouri <atef.nouri@gmail.com>
2025-07-29 21:16:47 +0100

    feat:footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SkillsComponent } from './skills/skills.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, PortfolioComponent , SkillsComponent, ContactComponent],
+  imports: [HomeComponent, PortfolioComponent , SkillsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
