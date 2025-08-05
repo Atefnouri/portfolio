d7f22897467d4601f2953e3631382e3c9de51e18
Atef Nouri <atef.nouri@gmail.com>
2025-08-05 13:25:26 +0100

    refactor: update skills component to use @for loop
    Update the skills component to use the new @for control flow syntax from Angular 17. This replaces the previous implementation that used a static template.
    
    feat: add skills array to skills component
    
    Add a new skills array to the skills component to store skill data.

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      imageUrl: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
+      title: 'Skill 1'
+    },
+    {
+      imageUrl: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp',
+      title: 'Skill 2'
+    },
+    {
+      imageUrl: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp',
+      title: 'Skill 3'
+    }
+  ];
 }
